281c027939c6d58eafb15620869ff4264022cac1
sycodes95 <sycodes95@gmail.com>
2023-01-06 20:16:55 -0800

    changed port

--- express-generator/app.js
+++ express-generator/app.js
@@ -41,7 +41,7 @@ app.use(function(err, req, res, next) {
 });
 
 
-
-app.listen(process.env.PORT || "8080")
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
 
 module.exports = app;
